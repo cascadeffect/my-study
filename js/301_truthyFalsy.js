de9d1ec313918ad753921083c9f8e7374a358798
Cade <chyeon81@icloud.com>
2023-03-17 20:29:19 +0900

    Update

--- js/301_truthyFalsy.js
+++ js/301_truthyFalsy.js
@@ -1,7 +1,7 @@
 /*
 Truthy: JS에서 true가 아니어도 true로 취급되는 값
-  e.g., [], {}, Infinity, ' ', " " 등을 포함하여 Falsey를 제외한 모든 값
-Falsey: JS에서 false가 아니어도 false로 취급되는 값
+  e.g., [], {}, Infinity, ' ', " " 등을 포함하여 Falsy를 제외한 모든 값
+Falsy: JS에서 false가 아니어도 false로 취급되는 값
   e.g., null, undefined, 0, -0, NaN, '', ""
 */
 
